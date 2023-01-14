3371f09365813e0e2cc77406d52238faed56e643
apple <apple@MacBook-Pro.local>
2023-01-14 21:55:26 +0100

    initial commit

---
+++ TabViewDemo/TabViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewDemoApp.swift
+//  TabViewDemo
+//
+//  Created by apple on 14/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct TabViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
